2124113ab4c41858cee6710a333eb4f1f82137db
Kye Buerkle <22kyebue@gmail.com>
2026-02-02 14:42:30 -0700

    roboflow_download.py based on config file

--- src/roboflow/download_dataset.py
+++ src/roboflow/download_dataset.py
@@ -7,6 +7,29 @@ import roboflow
 import argparse
 import sys
 import os
+import json
+
+# .json settings file path
+CONFIG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "roboflow_config.json")
+
+def load_json(path):
+    if not os.path.exists(path):
+        return {}
+    with open(path, "r") as f:
+        return json.load(f)
+
+def save_json(path, directory, workspace, project, version):
+	with open(path, "w") as f:
+		json.dump(
+			{
+				"directory": directory,
+				"workspace": workspace,
+				"project": project,
+				"version": version
+			},
+			f,
+			indent = 2
+			)
 
 #	@brief: logs into roboflow with api, or user
 #	@param: api_key, when none login with user
@@ -22,12 +45,15 @@ def roboflow_login(api_key = None):
 
 	except Exception as e:
 		print(f"Failed to log into Roboflow, exception: {e}")
+		sys.exit(1)
 		return None
 
-if __name__=="__main__":
-	#	default data path
-	dir_path = os.path.dirname(os.path.abspath(__file__))
-	data_path = os.path.abspath(f"{dir_path}/../../data")
+# --------- Main Function ----------
+def roboflow_download():
+	if os.path.exists(CONFIG_PATH):
+		config = load_json(CONFIG_PATH)
+	else:
+		config = {}
 
 	# parse the arguments
 	parser = argparse.ArgumentParser(
@@ -35,23 +61,53 @@ if __name__=="__main__":
 		description = "downloads a dataset from roboflow"
 		)
 
+	parser.add_argument('-d', "--directory", help = "output directory", default = config.get("directory"))
+	parser.add_argument('-w', "--workspace", help = "roboflow workspace", default = config.get("workspace"))
+	parser.add_argument('-p', "--project", help = "Project ID from rboflow", default = config.get("project"))
+	parser.add_argument('-v', "--version", help = "versions of project, default 1", type=int, default = config.get("version"))
 	parser.add_argument('-k', "--key", help = "api key for Roboflow login", default=None)
 	parser.add_argument('-f', "--format", help = "model format of images", default = "coco")
-	parser.add_argument('-d', "--directory", help = "output directory", default = data_path)
-	parser.add_argument('-w', "--workspace", help = "roboflow workspace")
-	parser.add_argument("project", help = "Project ID from rboflow")
-	parser.add_argument("version", help = "versions of project, default 1", type=int)
+	# TODO: parser.add_argument('-c', "--configuration", help = "configuration file", default = CONFIG_PATH)
 	args = parser.parse_args()
 
 	#	check parsed args
-	workspace = args.workspace.replace(" ", "-")
-	output_dir = os.path.join(args.directory, f"{args.project}-{args.version}")
+	directory_r = args.directory
+	workspace_r = args.workspace
+	project_r = args.project
+	version_r = args.version
+	# defaults if not in config or argument
+	if (directory_r is None) or (directory_r == ""):
+		dir_path = os.path.dirname(os.path.abspath(__file__))
+		directory_r = os.path.abspath(f"{dir_path}/../../data")
+	if (workspace_r is None) or (workspace_r == ""):
+		print("No 'workspace' in config, use -w to determine the workspace")
+		sys.exit(1)
+	if (project_r is None) or (project_r == ""):
+		print("No 'project' in config, use -p to determine the project")
+		sys.exit(1)
+	if (version_r is None) or (version_r == 0):
+		version_r = 1
 
-	#	login to roboflow
-	rf = roboflow_login(api_key= args.key)
-	project = rf.workspace(workspace).project(args.project)
-	version = project.version(args.version)
-	dataset = version.download(model_format = args.format, location = output_dir)
-	print(f"Path to dataset: {dataset.location}")
-	print(f"Dataset name: {dataset.name}")
-	print(f"Dataset version: {dataset.version}")
+	output_dir = os.path.join(directory_r, f"{project_r}-{version_r}")
+	if os.path.exists(output_dir):
+		print("Dataset already exists, Roboflow overwriting...")
+
+	#	login to roboflow & download dataset
+	try:
+		rf = roboflow_login(api_key= args.key)
+		project = rf.workspace(workspace_r).project(project_r)
+		version = project.version(version_r)
+		dataset = version.download(model_format = args.format, location = output_dir)
+		print(f"Path to dataset: {dataset.location}")
+		print(f"Dataset name: {dataset.name}")
+		print(f"Dataset version: {dataset.version}")
+	except Exception as e:
+		print(f"Failed to download dataset from config file: {CONFIG_PATH}\n {e}")
+		sys.exit(1)
+
+	#	saving new json config
+	save_json(CONFIG_PATH, directory_r, workspace_r, project_r, version_r)
+	return dataset
+
+if __name__ == "__main__":
+	roboflow_download()
