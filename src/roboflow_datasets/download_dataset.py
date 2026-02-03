7748bb21497a2afb40303e64489c45c292b1ee29
Kye Buerkle <22kyebue@gmail.com>
2026-02-02 18:41:41 -0700

    refactored download into a library to ustilize functions in upload

--- src/roboflow_datasets/download_dataset.py
+++ src/roboflow_datasets/download_dataset.py
@@ -3,51 +3,11 @@
 #	brief: downloads a roboflow dataset
 #	usage: download_dataset.py [options]
 
-import roboflow
 import argparse
 import sys
 import os
-import json
 import shutil
-
-# .json settings file path
-CONFIG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "roboflow_config.json")
-
-def load_json(path):
-    if not os.path.exists(path):
-        return {}
-    with open(path, "r") as f:
-        return json.load(f)
-
-def save_json(path, directory, workspace, project, version):
-	with open(path, "w") as f:
-		json.dump(
-			{
-				"directory": directory,
-				"workspace": workspace,
-				"project": project,
-				"version": version
-			},
-			f,
-			indent = 2
-			)
-
-#	@brief: logs into roboflow with api, or user
-#	@param: api_key, when none login with user
-#	@return: roboflow.Roboflow -> rf for roboflow login
-def roboflow_login(api_key = None):
-	try:
-		if (api_key != None):
-			rf = roboflow.Roboflow(api_key = api_key)
-		else:
-			roboflow.login()
-			rf = roboflow.Roboflow()
-		return rf
-
-	except Exception as e:
-		print(f"Failed to log into Roboflow, exception: {e}")
-		sys.exit(1)
-		return None
+from dataset_module import CONFIG_PATH, load_json, roboflow_login, save_json
 
 # --------- Main Function ----------
 def roboflow_download():
